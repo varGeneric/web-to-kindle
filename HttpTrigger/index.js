8b0c3e8ee1636cb7340a3c24fbe1f6466f4c83f5
varGeneric <16168572+varGeneric@users.noreply.github.com>
2022-01-11 02:18:26 -0500

    Use js image manipulation lib

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
-const execFile = require('child_process').execFile;
-const fs = require('fs');
+const sharp = require('sharp');
 
 module.exports = async function (context, req) {
     const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
@@ -10,31 +9,40 @@ module.exports = async function (context, req) {
     await page.screenshot({
       path: '/tmp/screenshot.png',
     });
+    const screenshotBuffer = 
+        await page.screenshot({ fullPage: true });
 
     await browser.close();
+    
+    const manipulatedBuffer = 
+        await sharp(screenshotBuffer)
+          .grayscale()
+          .toColourspace('grey16')
 
-    await convert('/tmp/screenshot.png');
-    screenshot = fs.readFileSync('/tmp/screenshot.png');
 
-    context.res.writeHead(200, {
-     'Content-Type': 'image/png',
-     'Content-Length': screenshot.length,
-    });
-    return context.res.end(screenshot);
+    // await convert('/tmp/screenshot.png');
+    // screenshot = fs.readFileSync('/tmp/screenshot.png');
+
+    context.res = {
+      body: manipulatedBuffer,
+      headers: {
+        'Content-Type': 'image/png'
+      }
+    };
   }
 
 
 
-function convert(filename) {
-  return new Promise((resolve, reject) => {
-    const args = [filename, '-gravity', 'center', '-extent', '600x800', '-colorspace', 'gray', '-depth', '8', filename];
-    execFile('convert', args, (error, stdout, stderr) => {
-      if (error) {
-        console.error({ error, stdout, stderr });
-        reject();
-      } else {
-        resolve();
-      }
-    });
-  });
-}
+// function convert(filename) {
+//   return new Promise((resolve, reject) => {
+//     const args = [filename, '-gravity', 'center', '-extent', '600x800', '-colorspace', 'gray', '-depth', '8', filename];
+//     execFile('convert', args, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error({ error, stdout, stderr });
+//         reject();
+//       } else {
+//         resolve();
+//       }
+//     });
+//   });
+// }
